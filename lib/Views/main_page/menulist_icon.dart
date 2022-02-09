ce37ddeac0ce826e151561c0b8d9c6fa45a8553c
matsu <rice_shower_35@hotmail.com>
2022-02-09 18:01:06 +0900

    メニューリストの編集中

--- lib/Views/main_page/menulist_icon.dart
+++ lib/Views/main_page/menulist_icon.dart
@@ -22,7 +22,14 @@ class MenuListIcon extends StatelessWidget {
           builder: (BuildContext contex) {
             return SingleChildScrollView(
               child: Container(
-                child: _menuList(context),
+                child: Column(
+                  children: [
+                    // リスト更新
+                    ListUpdate(),
+                    // リスト削除
+                    ListDelete(),
+                  ],
+                ),
               ),
             );
           },
@@ -33,121 +40,127 @@ class MenuListIcon extends StatelessWidget {
 }
 
 ///
-/// showModalBottomSheet の表示内容
+/// リスト更新
 ///
-Widget _menuList(BuildContext context) {
-  final prvShared = context.watch<ProviderSharedPreferences>();
-  final store = context.watch<ProviderGroup>();
+class ListUpdate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final store = context.watch<ProviderGroup>();
 
-  return Column(
-    children: [
-      // グループリスト名の変更
-      Container(
-        margin: EdgeInsets.only(bottom: 25),
-        height: 60.0,
-        child: InkWell(
-          child: Row(
-            children: [
-              Padding(padding: EdgeInsets.only(left: 15.0)),
-              Text(
-                "リスト名を変更する",
-                style: TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-          onTap: () {
-            // グループリストタイトルを初期化
-            store.initTitleController(store.selectedTitle);
-            // "push"で更新画面に遷移
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return ListEditPage(MODE_UPD);
-                },
-              ),
-            ).then((value) async {
-              // storeの初期化
-              context.read<ProviderGroup>().clearItems();
-              // タイトルを反映させる
-              store.getSelectedInfo();
-              // グループリストの再読み込み
-              context.read<ProviderGroup>().initializeList();
-              // ToDoリストも再読み込みする
-              context.read<ProviderTodo>().initializeList();
-              // メニューリストを閉じる
-              Navigator.pop(context);
-            });
-          },
+    return Container(
+      margin: EdgeInsets.only(bottom: 25),
+      height: 60.0,
+      child: InkWell(
+        child: Row(
+          children: [
+            Padding(padding: EdgeInsets.only(left: 15.0)),
+            Text(
+              "リスト名を変更する",
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
         ),
-      ),
-      // グループリストの削除
-      Container(
-        margin: EdgeInsets.only(bottom: 25),
-        height: 60.0,
-        child: InkWell(
-          child: Row(
-            children: [
-              Padding(padding: EdgeInsets.only(left: 15.0)),
-              Text(
-                "リストを削除する",
-                style: TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-          onTap: () async {
-            var result = await showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("確認"),
-                  content: Container(
-                    height: 90.0,
-                    child: Column(
-                      children: [
-                        Text("リストを削除します。よろしいですか？"),
-                        Text(
-                          "この操作は取り消しできません。",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  actions: <Widget>[
-                    ElevatedButton(
-                      child: Text("Cancel"),
-                      onPressed: () => Navigator.of(context).pop(0),
-                    ),
-                    ElevatedButton(
-                      child: Text("OK"),
-                      onPressed: () {
-                        // グループリストと紐づくTodoを物理削除
-                        store.delete(store.selectedId);
-                        // デフォルトリストを選択中にする
-                        prvShared.saveValue(SELECT_ID_KEY, GROUPID_DEFUALT);
-                        prvShared.setSelectedGroupId(GROUPID_DEFUALT);
-                        // タイトルを反映させる
-                        store.getSelectedInfo();
-                        // ToDoリストも再読み込みする
-                        context.read<ProviderTodo>().initializeList();
-                        // ダイアログを閉じる
-                        Navigator.pop(context);
-                        // メニューリストを閉じる
-                        Navigator.pop(context);
-                      },
-                    )
-                  ],
-                );
+        onTap: () {
+          // グループリストタイトルを初期化
+          store.initTitleController(store.selectedTitle);
+          // "push"で更新画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return ListEditPage(MODE_UPD);
               },
-            );
-            print("dialog result: $result");
-          },
-        ),
+            ),
+          ).then((value) async {
+            // storeの初期化
+            context.read<ProviderGroup>().clearItems();
+            // タイトルを反映させる
+            store.getSelectedInfo();
+            // グループリストの再読み込み
+            context.read<ProviderGroup>().initializeList();
+            // ToDoリストも再読み込みする
+            context.read<ProviderTodo>().initializeList();
+            // メニューリストを閉じる
+            Navigator.pop(context);
+          });
+        },
       ),
-      // 下にスペース設ける
-      SpaceBox.height(50),
-    ],
-  );
+    );
+  }
+}
+
+///
+/// リスト削除
+///
+class ListDelete extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final prvShared = context.watch<ProviderSharedPreferences>();
+    final store = context.watch<ProviderGroup>();
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 25),
+      height: 60.0,
+      child: InkWell(
+        child: Row(
+          children: [
+            Padding(padding: EdgeInsets.only(left: 15.0)),
+            Text(
+              "リストを削除する",
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+        onTap: () async {
+          var result = await showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("確認"),
+                content: Container(
+                  height: 90.0,
+                  child: Column(
+                    children: [
+                      Text("リストを削除します。よろしいですか？"),
+                      Text(
+                        "この操作は取り消しできません。",
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                  ElevatedButton(
+                    child: Text("Cancel"),
+                    onPressed: () => Navigator.of(context).pop(0),
+                  ),
+                  ElevatedButton(
+                    child: Text("OK"),
+                    onPressed: () {
+                      // グループリストと紐づくTodoを物理削除
+                      store.delete(store.selectedId);
+                      // デフォルトリストを選択中にする
+                      prvShared.saveValue(SELECT_ID_KEY, GROUPID_DEFUALT);
+                      prvShared.setSelectedGroupId(GROUPID_DEFUALT);
+                      // タイトルを反映させる
+                      store.getSelectedInfo();
+                      // ToDoリストも再読み込みする
+                      context.read<ProviderTodo>().initializeList();
+                      // ダイアログを閉じる
+                      Navigator.pop(context);
+                      // メニューリストを閉じる
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              );
+            },
+          );
+          print("dialog result: $result");
+        },
+      ),
+    );
+  }
 }
